7bb38f71279e1859b28d1c310e39aaa8f27c5053
Subhabrata0710 <subchakraborty0710@gmail.com>
2026-06-22 16:26:01 +0530

    fa

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
   // ---- Configuration ----
   const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycby9oJakpKU62ZPIrYaRH_y-NL36fQe9r-SSAqAqwIuc3PWTU6f0_7hAh50ikzSxtVYlAg/exec',   // EYECOnic Apps Script URL
+    API_URL: 'https://script.google.com/macros/s/AKfycbw0mfAuW3Hgf2okE1BOjkHCu90BPheuW9MkYuUQUVSE9JfXCoGY2irQ-nwgPJ8f44PYIw/exec',   // EYECOnic Apps Script URL
     ANIMATION_THRESHOLD: 0.15,
     TOAST_DURATION: 4000,
     LOADER_DELAY: 600
